1085216eec76e0634597ce65ce679163895666bf
Malek Sharabi <m.malek1996@hotmail.com>
2025-10-27 15:28:05 +0100

    refactored the code to meet the solidity styling standard

--- src/Ownable.sol
+++ src/Ownable.sol
@@ -2,31 +2,31 @@
 pragma solidity ^0.8.0;
 
 contract Ownable {
-    address private _owner;
+    address private s_owner;
 
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
     constructor() {
-        _owner = msg.sender;
+        s_owner = msg.sender;
     }
 
     function owner() public view returns (address) {
-        return _owner;
+        return s_owner;
     }
 
     modifier onlyOwner() {
-        require(_owner == msg.sender, "You need to have owner priviliges to take this action");
+        require(s_owner == msg.sender, "You need to have owner priviliges to take this action");
         _;
     }
 
     function transferOwnership(address newOwner) public onlyOwner {
         require(newOwner != address(0), "Invalid ownership assignment");
-        emit OwnershipTransferred(_owner, newOwner);
-        _owner = newOwner;
+        emit OwnershipTransferred(s_owner, newOwner);
+        s_owner = newOwner;
     }
 
     function renounceOwnership() public onlyOwner {
-        emit OwnershipTransferred(_owner, address(0));
-        _owner = address(0);
+        emit OwnershipTransferred(s_owner, address(0));
+        s_owner = address(0);
     }
 }
--- src/Pausable.sol
+++ src/Pausable.sol
@@ -4,36 +4,36 @@ pragma solidity ^0.8.0;
 import {Ownable} from "./Ownable.sol";
 
 contract Pausable is Ownable {
-    bool private _paused;
+    bool private s_paused;
 
     event Paused(address account);
     event Unpaused(address account);
 
     constructor() {
-        _paused = false;
+        s_paused = false;
     }
 
     function paused() public view returns (bool) {
-        return _paused;
+        return s_paused;
     }
 
     modifier whenPaused() {
-        require(_paused, "Contract is not paused");
+        require(s_paused, "Contract is not paused");
         _;
     }
 
     modifier whenNotPaused() {
-        require(!_paused, "Contract is paused");
+        require(!s_paused, "Contract is paused");
         _;
     }
 
     function pause() public onlyOwner whenNotPaused {
-        _paused = true;
+        s_paused = true;
         emit Paused(msg.sender);
     }
 
     function unpause() public onlyOwner whenPaused {
-        _paused = false;
+        s_paused = false;
         emit Unpaused(msg.sender);
     }
 }

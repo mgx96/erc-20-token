8da3dcaa036a4e07798cdffdf774ccb80cc6d8b7
Malek Sharabi <m.malek1996@hotmail.com>
2025-10-27 16:40:15 +0100

    added a transfer check in the token contract and created a deployment script

---
+++ script/DeployToken.s.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Script} from "lib/forge-std/src/Script.sol";
+import {MyToken} from "../src/ERC-20 Token.sol";
+
+contract DeployToken is Script {
+    uint256 public constant INITIAL_SUPPLY = 1000_000;
+    function run() external {
+        vm.startBroadcast();
+        new MyToken(INITIAL_SUPPLY, "MyToken", "MTK");
+        vm.stopBroadcast();
+    }
+}
